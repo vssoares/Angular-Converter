2f2846e9dd8cbc3d6264dd7fb4f62d96a60d25c7
vssoares <vsss23@hotmail.com>
2020-07-24 19:28:13 -0300

    aaa

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
+
 const app = express();
- 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
- 
-app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + '/dist/AnyCalculator/'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/AnyCalculator/index.html'));
 });
- 
+
+// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
